4e9c28b86013fa037424d0c5385f03d9bc7f7420
KRISHNA RAJU <bandanabalakrishan5@gmail.com>
2026-08-19 20:43:53 +0530

    changes done

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.institute.user',
+  appName: 'Institute User',
+  webDir: 'build'
+};
+
+export default config;
